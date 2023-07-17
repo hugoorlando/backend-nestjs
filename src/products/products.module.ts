1b0c4b4d70b283b96bd070f31958122e78c8a120
Hugo Orlando Gonzalez <hugo-orlando.gonzalez@atos.net>
2023-07-17 10:59:43 -0300

    :rocket: added new features

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+import { Product, ProductImage } from './entities/index';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   // Asociar nuestra entity con el TypeORM de Postgres
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
 })
 export class ProductsModule {}
